c93a9ccc59b2cbc98b776373c5052c0fa03bc8b5
nijynot <nijynot@gmail.com>
2018-01-27 00:28:13 +0100

    Fix missing graph dir with fs-extra and add scripts to package.json

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const { printSchema } = require('graphql');
 const schema = require('../schema-es5/schema.js');
 
 const schemaPath = path.resolve(__dirname, '../graph/schema.graphql');
 
+fs.ensureFileSync(schemaPath);
 fs.writeFileSync(schemaPath, printSchema(schema));
 
 console.log(`Wrote: ${schemaPath}`);
