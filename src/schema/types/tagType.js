6186a42884ba9e56a62dc82f950deed83ceb6fd7
nijynot <nijynot@gmail.com>
2018-03-22 02:28:59 +0100

    Fix build bug, remove all Memory.js imports

--- src/schema/types/tagType.js
+++ src/schema/types/tagType.js
@@ -22,9 +22,7 @@ import { connectionFromArray } from '../definitions/connectionFromArray.js';
 import { transformToForward } from '../definitions/transformToForward.js';
 import { isOwner } from '../helpers.js';
 
-import { Memory } from '../models/Memory.js';
 import { Tag } from '../models/Tag.js';
-import { memoryType, memoryConnection } from './memoryType.js';
 
 export const tagType = registerType(new GraphQLObjectType({
   name: 'Tag',
@@ -40,23 +38,6 @@ export const tagType = registerType(new GraphQLObjectType({
           return Tag.count(context, rootValue.id);
         }
         return rootValue.count;
-      },
-    },
-    memories: {
-      type: memoryConnection,
-      args: {
-        ...connectionArgs,
-      },
-      resolve: async (rootValue, args, context) => {
-        const { first, after } = transformToForward(args);
-        const array = await mysql.getMemoryIdsByTagAndUserId({
-          tag: rootValue.tag,
-          user_id: context.user.user_id,
-          limit: first + 1,
-          offset: getOffsetWithDefault(after, -1) + 1,
-        })
-        .then(rows => rows.map(row => Memory.gen(context, row.id)));
-        return connectionFromArray(array, { first, after });
       },
     },
   }),
